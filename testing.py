ab1aeea215ee16ffa0417d2992b7389aeaf2de4d
alitariqureshi <alitariqureshi23@gmail.com>
2024-08-14 23:16:13 +0500

    helpinganoob.py

--- testing.py
+++ testing.py
@@ -1,5 +1,18 @@
 x = int(input('enter a number'))
+
 if(x & 1 == 0):
     print("It is even")
 else:
     print("It is odd")
+
+#zain tariq im a fan. Also code not that great u could do better learn from below
+
+boi = input("Are u a boi?")
+if boi == "Yes":
+    boi????????????????? = input("Are u sureeeeeeeeeeeeeeee??????")
+    if boi????????????????? == "Yes":
+        print("Wudda tusi andrew tate")
+else:
+    print("join bts. use forward link for help. "https://www.quora.com/How-can-I-possibly-join-BTS" ")
+
+    
